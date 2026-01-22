88db74e919460298ba46cbca5d72230700803a77
BRIGHT JAMES AFOLAMI <brightafolami@gmail.com>
2026-01-22 08:53:06 +0100

    Create patrick.js

---
+++ bin/patrick.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/**
+ * Patrick Push Protocol CLI Entry Point
+ * "We should take your problem and PUSH it somewhere else!"
+ * 
+ * @author Firestorm
+ * @version 1.0.0
+ */
+
+require('../src/cli.js');
